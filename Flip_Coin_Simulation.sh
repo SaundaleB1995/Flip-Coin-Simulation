b08677678b964935bbe3a7d6b6063c91c0884381
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-25 16:47:37 +0530

    [BHUSHAN] Added:Flip Coin Simulation showing Head or Tail Code.

--- Flip_Coin_Simulation.sh
+++ Flip_Coin_Simulation.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 echo "Welcome to Flip Coin Simulation "
+echo "Flip Coin Showing Head Or Tail"
+tossCoin=$((RANDOM%2))
+if [[ $tossCoin == 1 ]]
+then 
+    echo "-->Head."
+else
+    echo "-->Tail."
+fi
