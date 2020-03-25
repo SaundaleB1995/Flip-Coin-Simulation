6d0739ede6179d7d6ba2664deec9978aeb367114
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-25 17:00:08 +0530

    [BHUSHAN] Added:Flip the coin and Store in dictionary and show Head or Tail Percentage code.

--- Flip_Coin_Simulation.sh
+++ Flip_Coin_Simulation.sh
@@ -8,3 +8,39 @@ then
 else
     echo "-->Tail."
 fi
+
+
+echo "Enter a flip flop"
+number=10
+ta=0
+head=0
+
+for(( i=0; i<$number;i++))
+do
+   if [[ $((RANDOM%2)) -eq 1 ]]
+   then
+      
+      tail=`expr $tail + 1`
+      
+    else
+       
+       head=`expr $head + 1`
+       
+    fi 
+done
+
+declare -A TossCoin
+TossCoin[tail]=$tail 
+TossCoin[head]=$head
+
+h_percentage=$((100*$head/$number))
+
+echo "Head Percentage = " $h_percentage "%"
+
+t_percentage=$((100*$tail/$number))
+
+echo "Tail Percentage = " $t_percentage "%"
+
+echo "Head" ${TossCoin[head]}
+echo "Tail" 
+echo ${TossCoin[@]}
