277433795fbbaef50e7579c5d297ba70f4aff9d9
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-25 17:14:54 +0530

    [BHUSHAN] Added:Flip Double coin and Store in dictionary and show Head or Tail Percentage code.

--- Flip_Coin_Simulation.sh
+++ Flip_Coin_Simulation.sh
@@ -12,7 +12,7 @@ fi
 
 echo "Enter a flip flop"
 number=10
-ta=0
+tail=0
 head=0
 
 for(( i=0; i<$number;i++))
@@ -44,3 +44,42 @@ echo "Tail Percentage = " $t_percentage "%"
 echo "Head" ${TossCoin[head]}
 echo "Tail" 
 echo ${TossCoin[@]}
+
+declare -A DoubletDict
+DoubletDict+=(["HH"]=0 ["HT"]=0 ["TT"]=0 ["TH"]=0)
+echo "Doublet Combination"
+for ((i=0;i<$number;i++))
+do
+   check=$((RANDOM%4))
+       case $check in 
+          0)
+          DoubletDict["HH"]=$((${DoubletDict["HH"]}+1))
+          ;;
+          1)
+          DoubletDict["HT"]=$((${DoubletDict["HT"]}+1))
+          ;;
+          2)
+          DoubletDict["TT"]=$((${DoubletDict["TT"]}+1))
+          ;;
+          3)
+          DoubletDict["TH"]=$((${DoubletDict["TH"]}+1))
+          ;;
+        esac
+done
+for i in ${!DoubletDict[@]}
+do
+   DoubletDict[$i]=$(((${DoubletDict[$i]}*100)/$number))
+   done
+arrDoublet=($(for value in ${DoubletDict[@]}
+do
+   echo $value
+done | sort ))
+   max=${arrDoublet[3]}
+   echo ${arrDoublet[@]}
+      for key in ${!DoubletDict[@]}
+      do    
+           if [[ $max -eq ${DoubletDict[$key]} ]]
+           then 
+               echo "win combination $key"
+            fi
+       done
